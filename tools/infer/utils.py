37e83a922f0d4e412b98faf92c7b08d123224a58
shawn <shawn.zhao@intel.com>
2024-08-09 10:30:31 +0800

    Upload new fix with OV2024.3

---
+++ tools/infer/utils.py
@@ -0,0 +1,49 @@
+import numpy as np
+import yaml
+from easydict import EasyDict
+import torch
+
+def load_data_to_gpu(batch_dict):
+    for key, val in batch_dict.items():
+        if not isinstance(val, np.ndarray):
+            continue
+        if key in ['frame_id', 'metadata', 'calib', 'image_shape']:
+            continue
+        batch_dict[key] = torch.from_numpy(val).float()
+
+def merge_new_config(config, new_config):
+    if '_BASE_CONFIG_' in new_config:
+        with open(new_config['_BASE_CONFIG_'], 'r') as f:
+            try:
+                yaml_config = yaml.load(f, Loader=yaml.FullLoader)
+            except:
+                yaml_config = yaml.load(f)
+        config.update(EasyDict(yaml_config))
+
+    for key, val in new_config.items():
+        if not isinstance(val, dict):
+            config[key] = val
+            continue
+        if key not in config:
+            config[key] = EasyDict()
+        merge_new_config(config[key], val)
+
+    return config
+
+
+def get_cfg(cfg_file):
+    cfg = EasyDict()
+    cfg.LOCAL_RANK = 0
+    cfg_from_yaml_file(cfg_file, cfg)
+    return cfg
+
+
+def cfg_from_yaml_file(cfg_file, config):
+    with open(cfg_file, 'r') as f:
+        try:
+            new_config = yaml.load(f, Loader=yaml.FullLoader)
+        except:
+            new_config = yaml.load(f)
+        merge_new_config(config=config, new_config=new_config)
+
+    return config
